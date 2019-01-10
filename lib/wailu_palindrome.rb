fa7a5a8ebdb55cc62f453df140c875a275f93f5b
wailunlim <wailun@u.nus.edu>
2019-01-10 17:00:00 +0800

    enable palindrome functionality in integer

--- lib/wailu_palindrome.rb
+++ lib/wailu_palindrome.rb
@@ -1,16 +1,28 @@
 require "wailu_palindrome/version"
 
-class String
+module WailuPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
-  # Returns content for palindrome testing.
-  def processed_content
-    self.scan(/[a-z]/i).join.downcase
-  end
+    # Returns content for palindrome testing.
+    def processed_content
+      self.to_s.scan(/[a-z0-9]/i).join.downcase
+    end
+end
+
+class String
+  include WailuPalindrome
+end
+
+class Integer
+  include WailuPalindrome
 end
