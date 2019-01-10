4ff71eac7e71ccd0f5d94720c79e9d615c83f74d
wailunlim <wailun@u.nus.edu>
2019-01-10 15:49:31 +0800

    add tests

--- test/wailu_palindrome_test.rb
+++ test/wailu_palindrome_test.rb
@@ -17,4 +17,15 @@ class WailuPalindromeTest < Minitest::Test
   def test_palindrome_with_punctuation
     assert "Madam, I'm Adam.".palindrome?
   end
+
+  def test_letters
+    assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
+  end
+
+  def test_palindrome_checking_does_not_affect_string
+    value = "Madam, I'm Adam."
+    default = value
+    value.palindrome?
+    assert_equal default, value
+  end
 end
